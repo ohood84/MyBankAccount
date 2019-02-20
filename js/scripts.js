ce3167bf91102a49654862e308f43e95d05dc64e
Aya Abu <someone990@yahoo.com>
2019-02-20 00:16:23 -0800

    add the counters to weigh actors vs actress and vesa vers

--- js/scripts.js
+++ js/scripts.js
@@ -1,48 +1,77 @@
 $("document").ready(function(){
-  $("#submit").click(function(event){
 
-    event.preventDefault();
+    var names = [jen aniston,taylor swift,jen lawrence,cameron dios,
+                jimcarrey,clooney,chrisevan, jamesSpader];
+    var counters = [0,0,0,0,0,0,0,0];
 
     var name = $("input#name").val();
-    var intrested-in = $("input:radio[name=intrested-in]:checked").val();
-    var man-type = $("input:radio[name=man-type]:checked").val();
 
-    var woman-type = $("input:radio[name=woman-type]:checked").val();
-    var vacation-spot = $("input:radio[name=vacation-spot]:checked").val();
+    var womanType = $("input:radio[name=woman-type]:checked").val();
+    var vacationSpot = $("input:radio[name=vacation-spot]:checked").val();
     var intrests = $("input:radio[name=intrests]:checked").val();
-    var fav-color= $("input:radio[name=colors]:checked").val();
-    var show-kind = $("input:radio[name=show-kind]:checked").val();
-
-    var jenaniston-counter = 0;
-    var taylorswift-counter = 0;
-    var jenlawrence-counter = 0;
-    var jen-counter = 0;
-    var jimCarrey-counter = 0;
-    var clooney-counter = 0;
-    var chrisevans-counter = 0;
-    var jamesSpader-counter =0;
+    var favColor= $("input:radio[name=colors]:checked").val();
+    var showKind = $("input:radio[name=show-kind]:checked").val();
 
 
+    $(".intrested-in").click(function(){
+      var intrestedIn = $("input:radio[name=intrested-in]:checked").val();
 
-    if(animal === "cats"){
-      $("#c").show();
-      $("#s").hide();
-      $("#b").hide();
-    }
+      if(intrestedIn === "actor"){
+        $("#man-type").show();
+        $("#woman-type").hide();
+        counters[4]+=1000;
+        counters[5]+=1000;
+        counters[6]+=1000;
+        counters[7]+=1000;
+      }
+      else {
+        $("#woman-type").show();
+        $("#man-type").hide();
+        counters[0]+=1000;
+        counters[1]+=1000;
+        counters[2]+=1000;
+        counters[3]+=1000;
+      }
+    });
 
-    else if(animal==="squirrels"){
-      $("#s").show();
-      $("#c").hide();
-      $("#b").hide();
-    }
+    // adding the logic for the sec question <fav charactar-men>
+    $(".man-type").click(function(){
+      var manType = $("input:radio[name=man-type]:checked").val();
+      if(manType === "ross"){
+        jimCarreyCounter++;
+        chrisevansCounter++;
+      }
 
-    else{
-      $("#b").show();
-      $("#s").hide();
-      $("#c").hide();
-    }
+      else if (manType === "joey") {
+      jimCarreyCounter++;
+      }
+      else {
+        chrisevansCounter++;
+      }
+    });
+    // adding the logic for the sec question <fav charactar-women>
+    $(".man-type").click(function(){
+      var manType = $("input:radio[name=man-type]:checked").val();
+      if(manType === "ross"){
+        jimCarreyCounter++;
+        chrisevansCounter++;
+      }
+
+      else if (manType === "joey") {
+      jimCarreyCounter++;
+      }
+      else {
+        chrisevansCounter++;
+      }
+    });
 
 
-  });
+
+
+
+
+
+
+
 
 });
