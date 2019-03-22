701942b50fff8f69da091278e762c4e7a7ed7679
ohood84 <obeidat.ohood@gmail.com>
2019-03-22 10:48:18 -0700

    it will add bank account with best update

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,16 @@
 $(document).ready(function() {
   var initialDeposit = 0;
-  var depositAmount = 0;
+  var depositAmount =0;
   var totalBalance=0.0;
+  var virtualTotal=0;
   var newAccount1 ;
   var name="";
-
+  var input;
   var withdrawalAmount = 0;
+  var index=0;
 
-resetFields();
+  resetFields();
+
     function BankAccount() {
       this.accounts = [];
     }
@@ -20,8 +23,8 @@ resetFields();
     function Account(name, initialDeposit) {
       this.name= name;
       this.initialDeposit = initialDeposit;
-      this.Adeposit=0.0;
-      this.Awithdraw=0.0;
+      this.Adeposit=0;
+      this.Awithdraw=0;
       this.totalBalance=0.0;
     }
 
@@ -32,80 +35,64 @@ resetFields();
       // debugger;
     }
 
-    Account.prototype.deposit= function(depositAmount) {
-      return this.Adeposit=this.initialDeposit + depositAmount;
+    Account.prototype.deposit= function(virtualTotal,depositAmount) {
+
+      return this.totalBalance=virtualTotal+ depositAmount;
     }
 
-    Account.prototype.withdrawal= function(withdrawalAmount) {
-      return this.totalBalance= this.Adeposit-withdrawalAmount;
+    Account.prototype.withdrawal= function(virtualTotal,withdrawalAmount) {
+      return this.totalBalance= virtualTotal-withdrawalAmount;
     }
 
     document.getElementById("initial").onkeyup=function(){
+
       name = $("input#name").val();
       initialDeposit = parseFloat($("input#initial").val());
       newAccount1 = new Account(name, initialDeposit);
-       // newAccount1.initial=
-       newAccount1.addInitial(initialDeposit);
-      // $("#balance").val(newAccount1.totalBalance);
+      newAccount1.totalBalance=newAccount1.addInitial(initialDeposit);
+      index=0;
       $("#balance").val("$"+(newAccount1.totalBalance).toFixed(2));
 
     }
 
-  document.getElementById("deposit").onkeyup=function(){
-        depositAmount = parseFloat($("input#deposit").val());
-      newAccount1.totalBalance=newAccount1.deposit(depositAmount);
-      // newAccount1.totalBalance=newAccount1.deposit-newAccount1.withdrawal;
-       // $("#balance").val(newAccount1.totalBalance);
-       $("#balance").val("$"+(newAccount1.totalBalance).toFixed(2));
+    document.getElementById("deposit").onkeyup=function(){
+      // input="input#deposit";
+      input = parseFloat($("input#deposit").val());
+        if((index===2)|(index===0)){
+          virtualTotal=newAccount1.totalBalance;
+        }
+
+        if(isNaN(input)){
+         depositAmount=0;
+        }
+        else{
+          depositAmount = input;
+        }
+
+         newAccount1.totalBalance=newAccount1.deposit(virtualTotal,depositAmount);
+         $("#balance").val("$"+(newAccount1.totalBalance).toFixed(2));
+         index=1;
     }
 
 
 
     document.getElementById("withdrawal").onkeyup=function(){
-        withdrawalAmount = parseFloat($("input#withdrawal").val());
-        newAccount1.totalBalance=newAccount1.withdrawal(withdrawalAmount);
-          // newAccount1.totalBalance=newAccount1.deposit-newAccount1.withdrawal;
-        $("#balance").val("$"+(newAccount1.totalBalance).toFixed(2));
-    }
-
-
-    $("form#bank").submit(function(event){
-      event.preventDefault();
-
-
-      var bank = new BankAccount();
-
-      var name = $("input#name").val();
-      initialDeposit = 0.0;
-      // parseInt($("input#initial").val());
-      depositAmount =0.0;
-      // parseInt($("input#deposit").val());
-      // alert( depositAmount);
-      withdrawalAmount =0.0;
-       // parseInt($("input#withdrawal").val());
-      // alert( withdrawalAmount);
-
-      // var newAccount1 = new Account(name, initialDeposit);
-      // newAccount1.addInitial(initialDeposit);
-
-      bank.addAccount(newAccount1);
-      // $("#result").append("total balance is:" + " " + newAccount1.totalBalance+"<br>");
-
-
-
-         // newAccount1.deposit(depositAmount);
-        // $("#result").append("total balance is:" + " " +newAccount1.totalBalance);
-
-
-
-         // newAccount1.withdrawal(withdrawalAmount);
-        // $("#result").append("total balance is:" + " " +newAccount1.totalBalance);
-
-
-       // $("#balance").val(newAccount1.totalBalance);
+        input=parseFloat($("input#withdrawal").val());
+        if((index===1)|(index===0)){
+          virtualTotal=newAccount1.totalBalance;
+        }
+        if(isNaN(input)){
+          withdrawalAmount=0;
+        }
+        else{
+          withdrawalAmount =input;
+        }
+          newAccount1.totalBalance=newAccount1.withdrawal(virtualTotal,withdrawalAmount);
+          $("#balance").val("$"+(newAccount1.totalBalance).toFixed(2));
+          index=2;
+      }
 
       resetFields();
-    });
 
 
 
@@ -114,34 +101,5 @@ resetFields();
     $("#balance").val("0.00");
   }
 
-    // $("form").not($("div form.new-address"));
-    //
-    //
-    // var inputtedFirstName = $("input#new-first-name").val();
-    // var inputtedLastName = $("input#new-last-name").val();
-    // var newContact = new Contact(inputtedFirstName, inputtedLastName);
-    //
-    // $(".new-address").each(function() {
-    //   var inputtedStreet = $(this).find("input.new-street").val();
-    //   var inputtedCity = $(this).find("input.new-city").val();
-    //   var inputtedState = $(this).find("input.new-state").val();
-    //   var newAddress = new Address(inputtedStreet, inputtedCity, inputtedState)
-    //   newContact.addresses.push(newAddress)
-    // });
-    //
-    // $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
-    //
-    // $(".contact").last().click(function() {
-    //   $("#show-contact").show();
-    //   $("#show-contact h2").text(newContact.fullName());
-    //   $(".first-name").text(newContact.firstName);
-    //   $(".last-name").text(newContact.lastName);
-    //   $("ul#addresses").text("");
-    //   newContact.addresses.forEach(function(address) {
-    //     $("ul#addresses").append("<li>" + address.fullAddress() + "</li>");
-    //   });
-    // });
 
-  //   resetFields();
-  // });
 });
